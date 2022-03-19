722733e4e827372d9ab46613e7d39f6a19181181
Muhammad Rafiuddin <71545866+rafimrfdn@users.noreply.github.com>
2022-03-20 07:15:11 +0800

    Update powermenu.sh

--- .scripts/powermenu.sh
+++ .scripts/powermenu.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-function powermenu 
-    options="Cancel\nLogout\nRestart\nShutdown"
-    selected=$(echo -e $options | rofi -dmenu -i -theme-str '@import "gnomepowermenu.rasi"')
-    if [ $selected = "Shutdown" ]; then
-        doas poweroff
-      elif [ $selected = "Restart" ]; then
-        doas init 6 
-      elif [ $selected = "Logout" ]; then
-        pkill -U $USER 
-      elif [ $selected = "Cancel" ]; then
-        return 
-    fi
+#function powermenu 
+#    options="Cancel\nLogout\nRestart\nShutdown"
+#    selected=$(echo -e $options | rofi -dmenu -i -theme-str '@import "gnomepowermenu.rasi"')
+#    if [ $selected = "Shutdown" ]; then
+#        doas poweroff
+#      elif [ $selected = "Restart" ]; then
+#        doas init 6 
+#      elif [ $selected = "Logout" ]; then
+#        pkill -U $USER 
+#      elif [ $selected = "Cancel" ]; then
+#        return 
+#    fi
 
 
-#chosen=$(printf "Shutdown\nRestart\nLogout" | rofi -dmenu -i -theme-str '@import "onedark.rasi"')
-#
-#case "$chosen" in
-#  "Shutdown" doas poweroff ;;
-#  "Restart" doas init 6 ;;
-#  "Logout" pkill -u $USER ;;
-#  *) exit 1 ;;
-#esac
+chosen=$(printf "Shutdown\nRestart\nLogout" | rofi -dmenu -i -theme-str '@import "onedarkpowermenu.rasi"')
+
+case "$chosen" in
+  "Shutdown") doas poweroff ;;
+  "Restart") doas init 6 ;;
+  "Logout") pkill -u $USER ;;
+  *) exit 1 ;;
+esac
